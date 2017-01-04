77f2680622acf80e42171847acf62e39cb7e9387
Jelmar Van Aert <jelmarvanaert@jelmars-air>
2017-01-04 15:51:49 +0100

    Initial Commit

---
+++ Github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Github
+//
+//  Created by Jelmar Van Aert on 04/01/2017.
+//  Copyright © 2017 Jelmar Van Aert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
