264fd99763512508502f9fcb56e392044b6f7400
Jelmar Van Aert <jelmarvanaert@jelmars-air.home>
2017-01-04 15:56:49 +0100

    Hello world

--- Github/ViewController.swift
+++ Github/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+         print("say hello world")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+   
 }
 
