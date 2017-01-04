04ea7e850bceb7baec7d33e06909f27f91aa4507
Jelmar Van Aert <jelmarvanaert@jelmars-air.home>
2017-01-04 16:15:52 +0100

    Hello world

--- Github/ViewController.swift
+++ Github/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
          print("say hello world")
+        print("say not hello world")
     }
 
     override func didReceiveMemoryWarning() {
